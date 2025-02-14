9eb73d0c4b19f9acee686518f828889d572db179
Olejjjka <oleggaming089@gmail.com>
2025-02-15 01:38:20 +0400

    Initial commit

---
+++ src/main/java/com/example/beauty_salon_booking/BeautySalonBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.beauty_salon_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautySalonBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautySalonBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/beauty_salon_booking/BeautySalonBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beauty_salon_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautySalonBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
